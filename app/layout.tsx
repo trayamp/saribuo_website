2fb2624a30c955c41528ac205710f7344a2019d7
Justin <itzyoboii13@gmail.com>
2025-07-03 14:33:37 +0800

    Fix layout head tag and remove unused Head import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
-import Head from "next/head"; // <-- Add this if needed (optional with app dir)
+
 
 const font = Roboto({
   weight: ["300", "400", "500", "600", "700"],
